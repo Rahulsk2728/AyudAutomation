345a1015da6fba83094d088e805e449e7f9eb995
rahulsk2728 <kshirsagar.rahul97@gmail.com>
2026-09-01 13:49:11 +0530

    GET and POST API

--- src/test/java/apiTests/UserAPITest.java
+++ src/test/java/apiTests/UserAPITest.java
@@ -3,6 +3,7 @@ package apiTests;
 import api.APIClient;
 import endpoints.UserEndpoints;
 import io.restassured.response.Response;
+import models.CreateUserRequest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,11 +12,74 @@ public class UserAPITest {
     @Test
     public void getAllUsersTest() {
 
-        Response response = APIClient.get(UserEndpoints.GET_ALL_USERS);
+        Response response =
+                APIClient.get(UserEndpoints.GET_ALL_USERS);
 
         System.out.println(response.asPrettyString());
 
-        Assert.assertEquals(response.getStatusCode(), 200);
+        // Status code
+        Assert.assertEquals(
+                response.getStatusCode(),
+                200,
+                "Status code is not 200"
+        );
 
+        // Response time
+        Assert.assertTrue(
+                response.getTime() < 3000,
+                "Response took more than 3 seconds"
+        );
+
+        // Response body validation
+        Assert.assertNotNull(
+                response.jsonPath().get("data"),
+                "Data is missing from response"
+        );
+
+        // Validate first user's ID
+        int firstUserId =
+                response.jsonPath().getInt("data[0].id");
+
+        Assert.assertTrue(
+                firstUserId > 0,
+                "Invalid user ID"
+        );
+    }
+
+
+    @Test
+    public void createUserTest() {
+
+        CreateUserRequest request =
+                new CreateUserRequest(
+                        "Rahul",
+                        "SDET"
+                );
+
+        Response response =
+                APIClient.post(
+                        UserEndpoints.CREATE_USER,
+                        request
+                );
+
+        System.out.println(
+                response.asPrettyString()
+        );
+
+        Assert.assertEquals(
+                response.getStatusCode(),
+                201,
+                "Expected status code 201"
+        );
+
+        Assert.assertEquals(
+                response.jsonPath().getString("name"),
+                "Rahul"
+        );
+
+        Assert.assertEquals(
+                response.jsonPath().getString("job"),
+                "SDET"
+        );
     }
 }
